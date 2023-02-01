9c7807b650cadc34b8a1870d2858d1a86a10de8b
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-02-01 14:52:07 +0530

    FEAT: create user

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -27,3 +27,44 @@ module.exports.signup = function(req, res) {
         return res.render('emp_sign_up');
     }
 }
+
+module.exports.create = async function(req, res) {
+    if (req.body.password != req.body.confirmPassword) {
+        return res.redirect('back');
+    }
+
+    Employee.findOne({ email: req.body.email }, function(err, emp) {
+        if (err) {
+            console.log('Error in finding user');
+            return;
+        }
+        if (!emp) {
+            Employee.create(req.body, async function(err, emp) {
+                if (err) {
+                    console.log('Error in creating the user', err);
+                    return;
+                }
+                Employee.count({}, async function(err, count) {
+                    console.log('Number of Employees: '+ count);
+                    if (count == 1) {
+                        emp.isAdmin = "true";
+                        await emp.save();
+                        console.log('Promoted as admin');
+                    }
+                    else {
+                        emp.isAdmin = "false";
+                        await emp.save();
+                        console.log('Promoted as employee');
+                    }
+                });
+                console.log('Employee created successfully');
+
+                return res.render('emp_sign_in');
+            })
+        }
+        else {
+            console.log('Employee Exists');
+            return res.render('emp_sign_in');
+        }
+    })
+}
