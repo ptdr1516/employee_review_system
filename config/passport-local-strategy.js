6862045a418166669aa42f25bf333221eac22437
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-02-01 14:22:00 +0530

    passport local strategy

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,2 +1,22 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const Employee = require('../models/employee');
+
+// local strategy for user's credentials that are verified against the data stored in the database.(in this case email is required as username)
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+},
+function(email, password, done) {
+    Employee.findOne({ email: email }, function(err, emp) {
+        if (err) {
+            console.log('Error in finding the user');
+            return done(err);
+        }
+        if (!emp || emp.password != password) {
+            console.log('Invalid username or password');
+            return done(null, false);
+        }
+
+        return done(null, emp);
+    });
+}));
