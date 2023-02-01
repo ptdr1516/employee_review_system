abc91bf810acf3a448312e99eea3877c38dc6206
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-02-01 14:41:31 +0530

    FEAT: home, signin

---
+++ controllers/homeController.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Employee = require('../models/employee');
+
+
+module.exports.home = function(req, res) {
+    let Emp = req.user;
+
+    return res.render('home', {
+        emp: Emp,
+    });
+}
+
+module.exports.signin = function(req, res) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/');
+    }
+    else {
+        return res.render('emp_sign_in');
+    }
+}
