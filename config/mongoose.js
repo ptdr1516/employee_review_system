d28edc527d497a8be75c34d0f2a48a5c87942d4e
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-02-05 13:14:51 +0530

    upates

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // establishes a connection to a MongoDB database.
 mongoose.connect('mongodb://localhost/Employee-Review-System');
-
+mongoose.set('strictQuery', false);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind('Error while connecting to the database'));
